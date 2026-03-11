202e5b697eaff11be46a529cad585e0b8c1d09e5
Roy <mutuaroymulinge@gmail.com>
2026-03-11 23:38:19 +0300

    add projects import in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -25,6 +25,15 @@ function App() {
             </ProtectedRoute>
           }
         />
+        
+        <Route
+          path="/projects"
+          element={
+            <ProtectedRoute>
+              <Projects />
+            </ProtectedRoute>
+          }
+        />
 
        </Routes>
 
