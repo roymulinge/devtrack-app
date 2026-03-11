b0c4e665fc193529dfc4bb41669d9c9934b4b765
Roy <mutuaroymulinge@gmail.com>
2026-03-11 13:31:44 +0300

    import register in app

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <Navbar />
        <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
        </Routes>
     </Router>
       
