cba023d5e2a903074c02b1d2d6f53c5ab86fd9dc
Roy <mutuaroymulinge@gmail.com>
2026-03-16 13:01:44 +0300

    feat(main): Wrap app with ThemeProvider

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from "./context/AuthContext.jsx";
+import { ThemeProvider } from "./context/ThemeContext.jsx";
 import { BrowserRouter } from 'react-router-dom';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <AuthProvider>
+     <ThemeProvider>
+       <AuthProvider>
         <App />
       </AuthProvider>
+     </ThemeProvider>
+      
     </BrowserRouter>
   </StrictMode>
 )
