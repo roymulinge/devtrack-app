8140735b4eaa26803d1ddbb5da734257ebe3ec4d
Roy <mutuaroymulinge@gmail.com>
2026-04-09 14:11:48 +0300

    fix(app) remove router

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,8 @@ import { useContext } from "react";
 import { BrowserRouter as Router,  } from "react-router-dom";
 
 function App() {
-  return (
-    <Router>
-      <AppContent />
-    </Router>
-  );
+  return  <AppContent />;
+    
 }
 
 export default App
